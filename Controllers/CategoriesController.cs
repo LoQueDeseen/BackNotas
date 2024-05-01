22242aa1ca23c96165a3d0fdfebcbba6c2086dc1
jkeynerfernandez <fernandezdj54@gmail.com>
2024-05-01 13:34:26 -0500

    correcion de controlador

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -1,7 +1,10 @@
+
 using BackNotas.Data;
 using Microsoft.AspNetCore.Mvc;
  
 namespace BackNotas.Controllers{
+    [Route ("api/[controller]")]
+    [ApiController]
 
     public class CategoriesController:Controller{
 
