9bf13a56c2de45ada37a1a7ea83638409253d9ec
jkeynerfernandez <fernandezdj54@gmail.com>
2024-05-01 14:00:07 -0500

    controlador para encontrar categorìa por id

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -16,10 +16,22 @@ namespace BackNotas.Controllers{
             _context= context;
         }
 
-        [HttpGet]
+        [HttpGet]// traigo lista de categorías
         public async Task<ActionResult<IEnumerable<Category>>> GetUser(){ //Ienumerable trae una coleccion de categorías 
             return await _context.Categories.ToListAsync();// trae la lista de categorias en la base de datos 
         }
+
+        [HttpGet("{Id}")] 
+        public async Task<ActionResult<Category>> GetCategory(int Id){ 
+            var category= await _context.Categories.FindAsync();// trae la que encuantre por id
+
+            if (category==null){ // si no llega datos, retorna no encontrado 
+                return NotFound();
+
+            }
+            return category;
+        }
+
 
 
     }
