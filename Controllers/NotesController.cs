1072e110ae412403f988e70ee7addec95e90c969
Davidhzk2 <guffyherrera150@gmail.com>
2024-05-01 19:08:50 -0500

    CRUD de notas

--- Controllers/NotesController.cs
+++ Controllers/NotesController.cs
@@ -1,6 +1,8 @@
 
 using BackNotas.Data;
+using BackNotas.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BackNotas.Controllers
 {
@@ -15,11 +17,57 @@ namespace BackNotas.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
-        {
-            return View();
+        [HttpGet] //método que trae elementos de las notas
+        public async Task<ActionResult<IEnumerable<Note>>> GetNotes(){
+
+            return await _context.Notes.ToListAsync();
         }
 
+        //Detalles de notas 
+        [HttpGet]
+        public async Task<ActionResult<Note>> GetNote(int id){
+            var note = await _context.Notes.FindAsync(id);
+
+            if(note == null){
+                return NotFound();
+            }
+            return note;
+        }
+
+        //Crear nota
+        [HttpPost]
+        public async Task <ActionResult<Note>> CreateNote(Note note){
+
+             _context.Add(note);
+            await _context.SaveChangesAsync();
+            
+            return CreatedAtAction("GetNote", new {id = note.Id}, note); //redicion a la action de get note
+        }
+
+        //Eliminar Nota 
+        [HttpDelete("{id}")] //eliminar por el id 
+        public async Task<IActionResult> DeleteNote(int id){
+
+            var note = await _context.Notes.FindAsync(id); //buscar nota por su id
+            if(note == null){
+                return  NotFound();
+            }
+            _context.Notes.Remove(note);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+        //Editar la nota
+        [HttpPut("{Id}")]
+
+        public async Task <IActionResult> PutNote(int Id, Note note){ 
+            _context.Entry(note).State = EntityState.Modified; // ingresa a  la categoria en la base de datos y se establece el estado a modificado
+            await _context.SaveChangesAsync();
+
+            return NoContent(); // respuesta 204
+        }
        
     }
 }
